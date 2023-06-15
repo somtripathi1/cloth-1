3b1adb8a92d86cdcf6b69de10aa4bfc63f8456ac
Prateek Vishwakarma <prateekv2003@gmail.com>
2023-06-15 12:52:53 +0530

    second commit

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
-import {ReactToPrint} from 'react-to-print';
-import config from '../config/config';
 import state from '../store';
-import { download } from '../assets';
-import { downloadCanvasToImage, reader } from '../config/helpers';
+import { reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
@@ -90,9 +87,6 @@ const Customizer = () => {
           state.isLogoTexture = !activeFilterTab[tabName];
         break;
       case "stylishShirt":
-          state.isFullTexture = !activeFilterTab[tabName];
-        break;
-        case "stylishShirt":
           state.isFullTexture = !activeFilterTab[tabName];
         break;
       default:
